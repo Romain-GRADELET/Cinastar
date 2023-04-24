b521a7e6de3c88efb836990a09de9e9a200b039d
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-04-24 15:09:04 +0200

    WIP twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,21 +4,26 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     /**
-     * Page par défaut
-     *  
-     * La route se définit sur la ligne ci-dessous (Attention ce n'est pas un commentaire)
-     * @Route("/radium", name="default", methods ={"GET", "POST"})
+     * page par défaut
+     *
+     * @Route("/radium", name="default", methods={"GET", "POST"})
      * 
      * @return Response
      */
     public function home(): Response
     {
-        $firstResponse = new Response('<h1>Salut les radiums</h1>');
-        return $firstResponse;
+        // la méthode render() prend 2 paramètres:
+        // * le nom du fichier de vue que l'on veux utiliser
+        // le chemin du fichier tiwg commence dans le dossier templates
+        // * un tableau de donnée à afficher
+        // cette méthode renvoit un objet Reponse, on va pouvoir le renvoyer
+        $twigResponse = $this->render("main/home.html.twig" /** + ViewData optionnel */);
+        
+        return $twigResponse;
     }
 }
-
