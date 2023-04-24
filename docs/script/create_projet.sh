737300185d5fbc1423e7286c5ea0866740f4b593
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-04-24 12:06:32 +0200

    Commande sh

---
+++ docs/script/create_projet.sh
@@ -0,0 +1,9 @@
+# E02 : création du projet
+## création du projet symfony avec composer
+composer create-project symfony/skeleton:"^5.4" my_project_directory
+## déplacement des fichiers à la racine
+mv ./my_project_directory/* ./my_project_directory/.* .
+## suppression du dossier temporaire
+rm -r ./my_project_directory
+
+# E03
