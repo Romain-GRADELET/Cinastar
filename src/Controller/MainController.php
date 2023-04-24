1b95e189cddf8f864adb0054983c8589a3a506f6
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-04-24 17:41:06 +0200

    Création des routes et controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController extends AbstractController
     /**
      * page par défaut
      *
-     * @Route("/radium", name="default", methods={"GET", "POST"})
+     * @Route("/", name="default", methods={"GET", "POST"})
      * 
      * @return Response
      */
@@ -22,8 +22,51 @@ class MainController extends AbstractController
         // le chemin du fichier tiwg commence dans le dossier templates
         // * un tableau de donnée à afficher
         // cette méthode renvoit un objet Reponse, on va pouvoir le renvoyer
-        $twigResponse = $this->render("main/home.html.twig" /** + ViewData optionnel */);
+        $twigResponse = $this->render("main/index.html.twig" /** + ViewData optionnel */);
         
+        return $twigResponse;
+    }
+
+    /**
+     * Page des résultats de recherche
+     * 
+     * @Route("/find", name="list_movie", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function list(): Response
+    {
+        $twigResponse = $this->render("main/list.html.twig");
+
+        return $twigResponse;
+    }
+
+    /**
+     * Page des résultats d'un film/série
+     * 
+     * @Route("/movie/{id}", name="show_movie", methods={"GET"}, requirements={"id"="\d+"})
+     *
+     * @return Response
+     */
+    public function show(): Response
+    {
+        $twigResponse = $this->render("main/show.html.twig");
+
+        return $twigResponse;
+    }
+
+
+    /**
+     * Page des favoris
+     * 
+     * @Route("/favorites", name="favorites_movies", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function favorites(): Response
+    {
+        $twigResponse = $this->render("main/favorites.html.twig");
+
         return $twigResponse;
     }
 }
