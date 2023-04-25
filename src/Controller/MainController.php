10dcb496188f845aa9fd58091c6adeb3ddd15ced
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-04-25 14:11:37 +0200

    dynamisation show

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Models\MovieModel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,17 +18,23 @@ class MainController extends AbstractController
      */
     public function home(): Response
     {
+
+        // TODO : récuperer la liste de tout les films
+        // ! On utilise MovieModel tant que l'on a pas de BDD
+        $allMovies = MovieModel::getAllMovies();
+        //dd($allMovies);
+
         // la méthode render() prend 2 paramètres:
         // * le nom du fichier de vue que l'on veux utiliser
         // le chemin du fichier tiwg commence dans le dossier templates
         // * un tableau de donnée à afficher
         // cette méthode renvoit un objet Reponse, on va pouvoir le renvoyer
-        $demoData = "Bonjour du mardi matin";
+    
         $twigResponse = $this->render("main/home.html.twig",
         [
             // les données se passe par un tableau associatif
             // la clé du tableau deviendra le nom de la variable dans twig
-            "monTexteKilEstBo" => $demoData
+            "movieList" => $allMovies
         ]);
         
         return $twigResponse;
@@ -55,8 +62,12 @@ class MainController extends AbstractController
      */
     public function show($id): Response
     {
-        $twigResponse = $this->render("main/show.html.twig", [
-            "movieId" => $id
+        $movie = MovieModel::getMovie($id);
+
+        $twigResponse = $this->render("main/show.html.twig", 
+        [
+            "movieId" => $id,
+            "movieForTwig" => $movie
         ]);
 
         return $twigResponse;
