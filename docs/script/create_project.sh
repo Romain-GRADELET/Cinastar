8208aef5024a11050f0f50e9fab908b7aff00040
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-04-25 14:29:31 +0200

    mise à jour script création de projet

--- docs/script/create_project.sh
+++ docs/script/create_project.sh
@@ -1,9 +1,23 @@
 # E02 : création du projet
+
 ## création du projet symfony avec composer
 composer create-project symfony/skeleton:"^5.4" my_project_directory
 ## déplacement des fichiers à la racine
 mv ./my_project_directory/* ./my_project_directory/.* .
 ## suppression du dossier temporaire
-rm -r ./my_project_directory
+rmdir ./my_project_directory
+## pour pouvoir utiliser @Route()
+composer require annotations
+## TWIG !
+composer require twig
+
+
 
 # E03
+
+## pour faire des liens correct dans twig vers nos assets
+composer require symfony/asset
+## vive le debug : la barre de debug avec le backoffice de debug : profiler
+composer require --dev symfony/profiler-pack
+## permet au dump de ne plus être dans la page, mais dans le WebDebugToolbar
+composer require --dev symfony/debug-bundle
