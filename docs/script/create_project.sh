b2728a29aebeebb99258f02284381e4acf0e5e70
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-05-10 18:10:51 +0200

    modif script create_project

--- docs/script/create_project.sh
+++ docs/script/create_project.sh
@@ -2,42 +2,46 @@
 
 ## création du projet symfony avec composer
 composer create-project symfony/skeleton:"^5.4" my_project_directory
+
 ## déplacement des fichiers à la racine
 mv ./my_project_directory/* ./my_project_directory/.* .
+
 ## suppression du dossier temporaire
 rmdir ./my_project_directory
+
 ## pour pouvoir utiliser @Route()
 composer require annotations
+
 ## TWIG !
 composer require twig
 
-# E09
 ## formulaire ET validation
 composer require form validator
 
-# E03
 ## pour faire des liens correct dans twig vers nos assets
 composer require symfony/asset
+
 ## vive le debug : la barre de debug avec le backoffice de debug : profiler
 composer require --dev symfony/profiler-pack
+
 ## permet au dump de ne plus être dans la page, mais dans le WebDebugToolbar
 composer require --dev symfony/debug-bundle
+
 ## le maker
 composer require --dev symfony/maker-bundle
+
 ## Fixtures
 composer require orm-fixtures --dev
 
 ## Faker
 composer require fakerphp/faker
+
 ## installation du provider picsum
 composer require --dev bluemmb/faker-picsum-photos-provider ^2.0
 
 ## Package sécurité CSRF
 composer require security-csrf 
 
-
-
-# E05
 ## installation de tout les composants pour Doctrine
 ## A METTRE EN DERNIER car il nous pose une question à laquelle on répond 'x'
 composer require symfony/orm-pack
