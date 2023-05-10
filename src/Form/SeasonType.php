67199fec8ec33cc3a502e96a4b13693f3219828b
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-05-10 11:46:27 +0200

    crud Season OK

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Movie;
 use App\Entity\Season;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +16,26 @@ class SeasonType extends AbstractType
     {
         $builder
             ->add('number')
+
             ->add('nbEpisodes')
-            ->add('movie')
+
+            ->add('movie', EntityType::class,[
+                "multiple" => false,
+                "expanded" => false, // false: dropdown / true: radiobutton
+                "class" => Movie::class,
+                "choice_label" => "title",
+                // ? https://symfony.com/doc/5.4/reference/forms/types/entity.html#query-builder
+                "query_builder" => function(EntityRepository $entityRepository){
+                    // TODO : requete perso : tri par titre
+                    // TODO : requete perso : que le type série
+                    return $entityRepository->createQueryBuilder('m')
+                        // ! Error: Class App\Entity\Movie has no field or association named type.name    
+                        // ->where("m.type.name = 'série'")
+                        ->join("m.type", "t")
+                        ->where("t.name = 'série'")
+                        ->orderBy('m.title', 'ASC');
+                }
+            ])
         ;
     }
 
