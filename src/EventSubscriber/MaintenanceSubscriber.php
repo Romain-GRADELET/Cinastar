fdd2f4f663771dc915fa5cf3528b933a4b4dd61c
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-05-22 09:50:48 +0200

    subscriber OK

--- src/EventSubscriber/MaintenanceSubscriber.php
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -9,13 +9,54 @@ class MaintenanceSubscriber implements EventSubscriberInterface
 {
     public function onKernelResponse(ResponseEvent $event): void
     {
+        //dd($event);
+        // on trouve dans l'argument $event :
 
+        // * request, avec pathInfo
+        // va nous servir pour tester la route et exclure certaine route
+
+        // * response, avec le content
+        $response = $event->getResponse();
+        $content = $response->getContent();
+        // dd($content);
+        // on cherche l'élement HTML où se placer : <div class="container">
+        // et on remplace cet élément par notre message
+        $modifiedContent = str_replace(
+            // notre recherche
+            '<div class="container">', 
+            // on remplace par ...
+            '<div class="container">
+                <div class="alert alert-danger">Maintenance prévue jeudi 25 mai à 17h00</div>
+            ',
+            // sur quel contenu
+            $content
+        );
+        // il reste à mettre à jour le contenu de la response
+        $response->setContent($modifiedContent);
+        
+        // ================version en une seule instruction =======================
+        /*
+        $event->getResponse()->setContent(str_replace(
+            // notre recherche
+            '<div class="container">', 
+            // on remplace par ...
+            '<div class="container">
+                <div class="alert alert-danger">Maintenance prévue jeudi 25 mai à 17h00</div>
+            ',
+            // sur quel contenu
+            $response->getContent()
+        ));
+        */ //=====================================================================
+
+        // comme la méthode renvoit void, pas de return à faire de notre coté.
     }
 
     public static function getSubscribedEvents(): array
     {
         
         return [
+            // En clé le nom de l'évènement 
+            // En valeur : le nom de la méthode à exécuter
             'kernel.response' => 'onKernelResponse',
         ];
     }
