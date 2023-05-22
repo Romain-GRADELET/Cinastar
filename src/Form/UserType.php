11940ed7035fed38ea4448d87d3d55c019b8953b
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-05-22 10:33:12 +0200

    exemple event form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,7 +8,11 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
@@ -18,11 +22,52 @@ class UserType extends AbstractType
             ->add('email', EmailType::class, [
                 "label" => "Email pour se logger"
             ])
+            // on utilise l'event avant de mettre les données dans le formulaire
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                // On récupère le form depuis l'event (pour travailler avec)
+                $builder = $event->getForm();
+                // On récupère le user mappé sur le form depuis l'event
+                $user = $event->getData();
 
-            ->add('password', PasswordType::class, [
-                "label" => "Le mot de passe"
-            ])
-                
+                // On conditionne le champ "password"
+                // Si user existant, il a id non null
+                if ($user->getId() !== null) {
+                    // Edit
+                    $builder->add('password', PasswordType::class, [
+                        // je ne veux pas que le formulaire mettes automatiquement à jour la valeur
+                        // je désactive la mise à jour automatique de mon objet par le formulaire
+                        // ? https://symfony.com/doc/5.4/reference/forms/types/form.html#mapped
+                        "mapped" => false,
+                        "label" => "le mot de passe",
+                        "attr" => [
+                            "placeholder" => "laisser vide pour ne pas modifier ..."
+                        ],
+                        // On déplace les contraintes de l'entité vers le form d'ajout
+                        'constraints' => [
+                            new Regex(
+                                "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/",
+                                "Le mot de passe doit contenir au minimum 8 caractères, une majuscule, un chiffre et un caractère spécial"
+                            ),
+                        ],
+                    ]);
+                } else {
+                    // New
+                    $builder->add('password', null, [
+                        // En cas d'erreur du type
+                        // Expected argument of type "string", "null" given at property path "password".
+                        // (notamment à l'edit en cas de passage d'une valeur existante à vide)
+                        'empty_data' => '',
+                        // On déplace les contraintes de l'entité vers le form d'ajout
+                        'constraints' => [
+                            new NotBlank(),
+                            new Regex(
+                                "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/",
+                                "Le mot de passe doit contenir au minimum 8 caractères, une majuscule, un chiffre et un caractère spécial"
+                            ),
+                        ],
+                    ]);
+                }
+            })
             ->add('roles', ChoiceType::class, [
                 "multiple" => true,
                 "expanded" => true,
@@ -40,7 +85,7 @@ class UserType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-            "attr" => ["novalidate" => 'novalidate']
+            "attr" => ["novalidate" => "novalidate"]
         ]);
     }
 }
