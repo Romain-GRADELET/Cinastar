0c76358d4247053f70780ec96f2419bee0120ff4
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-05-25 11:06:39 +0200

    cours E20 pré déploiement

---
+++ tests/Back/UserAccessTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\Back;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAccessTest extends WebTestCase
+{
+    /**
+     * test url
+     *
+     * @dataProvider getUrls
+     * 
+     * @param string $url
+     */
+    public function testBackForbidden($url, $email): void
+    {
+        $client = self::createClient();
+
+        // Le Repo des Users
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        // On récupère admin@admin.com
+        $testUser = $userRepository->findOneByEmail($email);
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $client->request('GET', $url);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+
+    /**
+     * fournit TOUT les paramètres pour une function
+     * 
+     * @return array
+     */
+    public function getUrls()
+    {
+        yield ['/back/main', 'user@user.com'];
+        yield ['/back/movie', 'user@user.com'];
+        yield ['/back/casting/new', 'user@user.com'];
+        yield ['/back/casting/new', 'manager@manager.com'];
+        yield ['/back/casting/new', 'admin@admin.com'];
+    }
+}
